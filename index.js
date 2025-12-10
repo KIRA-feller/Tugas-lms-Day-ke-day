a24722377753b56386adb3159b82cef98798c5c1
kira <sitamvanh@gmail.com>
2025-12-10 22:20:21 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+
+app.get("/home", (req, res) => {
+    let nama = "muhamad Hikmat";
+    let lahir = "25/09/2005";
+    let tinggal = "Kota sukabumi"
+  res.render("index", {nama, lahir, tinggal});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
