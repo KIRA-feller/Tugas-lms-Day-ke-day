5199aafd1935c12535c27d6dc01d8dc002c0b8e9
Muhamad Hikmat <sitamvanh@gmail.com>
2025-12-17 23:33:49 +0700

    Menambahkan semua hasil coding terbaru

--- index.js
+++ index.js
@@ -2,13 +2,31 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+import { Pool } from 'pg'
+ 
+const db = new Pool({
+  user: 'postgres',
+  password: '123456',
+  host: 'localhost',
+  port: 5432,
+  database: 'data_anomali_fb',
+  max: 20
+})
+
 app.set("view engine", "hbs");
 app.set("views", "src/views");
 
 app.use("/assets", express.static("src/views/assets"));
+app.use(express.static("js"))
+app.use(express.urlencoded({extended: false}))
+
 
 // href versi express
 app.get("/home/:id", cHome)
+
+// form
+app.get("/project", projecT)
+app.post("/project", PProject)
 
 
 app.get("/", home);
@@ -20,22 +38,14 @@ app.listen(port, () => {
   let data = [
     {
       id:1,
-      title:"ini home"
+      title:"home"
     },
     {
       id:2,
-      title:"my project"
+      title:"contact me"
     },
     {
       id:3,
-      title:"contact me"
-    },
-    {
-      id:4,
-      title:"contact me"
-    },
-    {
-      id:5,
       title:"download cv"
     }
   ];
@@ -52,4 +62,29 @@ function cHome(req, res) {
   
 
   res.render("parameters", { result })
+}
+// batas
+
+async function projecT(req, res) {
+  const result = await db.query(
+    "SELECT * FROM urutan_anomaliii_ffb ORDER BY id ASC"
+  );
+
+  res.render("project", {
+    projects: result.rows
+  });
+}
+
+
+async function PProject(req, res) {
+  const { project_name } = req.body;
+
+  console.log("SUBMIT:", project_name);
+
+  await db.query(
+    "INSERT INTO urutan_anomaliii_ffb (name) VALUES ($1)",
+    [project_name]
+  );
+
+  res.redirect("/project");
 }
